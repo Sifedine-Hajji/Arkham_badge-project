1bc6760cc49ae4e566a111088f5b2c2a2436c614
denababaie <72995019+denababaie@users.noreply.github.com>
2021-01-20 09:52:33 +0100

    added footer & navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,11 +1,12 @@
-<nav>
-  <ul>
-  <?php
-    foreach($routes as $key=>$value){
-    ?>
-      <li><a href="?p=<?php echo $key; ?>"><?php echo $value[0]; ?></a></li>
-    <?
-    }
-  ?>
-  </ul>
-</nav>
+
+<!---------------------------------------NAVBAR----DASHBOARD---------------------------------------------->
+
+<div class="sidenav">
+    <div id=logomenu> <img src="../assets/images/logo.png">  </div>
+    <div class="menubutton">  <a href=""> <img src="../assets/images/homebutton.png"></a> </div>
+
+    <div class="menubutton"> <a href=""> <img src="../assets/images/gamersbutton.png"></a> </div>
+
+    <div id=logoutbutton> <a href="../pages/login.php"> <img src="../assets/images/logoutbutton.png"> </a>  </div>
+
+</div>
