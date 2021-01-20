e13955c43f31b591a5ff88ba33c829dae960d618
Sifedine <Sifedinehajji01@gmail.com>
2021-01-21 00:14:21 +0100

    dashboard

--- components/navbar.php
+++ components/navbar.php
@@ -1,11 +1,22 @@
 <!---------------------------------------NAVBAR----DASHBOARD---------------------------------------------->
 
-<div class="sidenav">
-    <div id=logomenu> <img src="../assets/images/logo.png">  </div>
-    <div class="menubutton">  <a href=""> <img src="../assets/images/homebutton.png"></a> </div>
 
-    <div class="menubutton"> <a href=""> <img src="../assets/images/gamersbutton.png"></a> </div>
 
-    <div id=logoutbutton> <a href="../pages/login.php"> <img src="../assets/images/logoutbutton.png"> </a>  </div>
-
-</div>
+    <div class="leftContainer">
+      <img src="../assets/logos/ARKHAM BADGES.png" alt="">
+      <ul>
+        <li>
+        <i class="fas fa-2x fa-home"></i>
+        <p>Home</p>
+        </li>
+        <li>
+        <i class="far fa-2x fa-user"></i>
+        <p>Users</p>
+        </li>
+        <li>
+        <i class="fas fa-2x fa-sign-out-alt"></i>
+        <p>logout</p>
+        </li>
+        
+      </ul>
+    </div>
--- pages/dashboardadmin.php
+++ pages/dashboardadmin.php
@@ -1,72 +1,79 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../assets/style.css" rel="stylesheet">
-    <title> Dashboard </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
 
-  <body class=DashboardBody>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../assets/style.css" rel="stylesheet">
+  <title> Dashboard </title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/08f226ae60.js" crossorigin="anonymous"></script>
+</head>
 
-
-<!--------------------------------------------NAVBAR------------------------------------------------------->
-
-<?php 
-    include("../components/navbar.php");
-?>
-
-<!------------------------------------------LIST--OF-BADGES------------------------------------------------>
-
-<div class= dashboardbatmanimage>
-
-<div class= dashboardbatmanfilter>
-</div>
-</div>
+<body class=DashboardBody>
 
 
-<ul class="list-group">
-  <h2>Badges</h2>
-   
-  <?php
-    include('../components/functions.php');
-    $getAllbadge = getAllBadges();
-    while($donnees = $getAllbadge->fetch()){
-  
-      echo'<li class="list-group-item d-flex justify-content-around">',
+  <!--------------------------------------------NAVBAR------------------------------------------------------->
+    <?php 
+      include("../components/navbar.php");
+    ?>
+
+  <!------------------------------LIST--OF-BADGES---------------------------------->
+  <section class="dashboardContainer">
       
-            '<img src=',$donnees['img_badges'],'>',
-            '<div class="nom_desc d-flex">','<p>',$donnees['nom_badges'],'</p>',
+      <div class="BaniereContainer">
+        <div class= dashboardbatmanimage>
+          <div class= dashboardbatmanfilter>
+            
+          </div>
+        </div>
+      </div>
+
+      <h2>Badges</h2>
+    <div class="mainContent">
+      <ul class="list-group">
+      
+      <?php
+      include('../components/functions.php');
+      $getAllbadge = getAllBadges();
+      while($donnees = $getAllbadge->fetch()){
+  
+        echo'<li class="list-group-item d-flex justify-content-around">',
+              '<img src=',$donnees['img_badges'],'>',
+              '<div class="nom_desc d-flex">','<p>',$donnees['nom_badges'],'</p>',
                     '<p>',$donnees['desc_badges'],'</p>',
-            '</div>',
-            '<div class="my-auto">',
-            '<button type="button" class="btn btn-dark">Edit</button>',
-            '</div>',
-          '</li>';        
+              '</div>',
+              '<div class="my-auto">',
+              '<button type="button" class="btn btn-dark">Edit</button>',
+              '</div>',
+            '</li>';        
+      }
+      ?>
+      </ul>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Create a new badge </h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-dark">Create badge </a>
+        </div>
+      </div>
+    </div>
+  </section>
 
-    }
+  <!------------------------------------------CREATE--A-BADGE------------------------------------------------>
 
-  ?>
-</ul>
-
-<!------------------------------------------CREATE--A-BADGE------------------------------------------------>
-
-<div class="card" style="width: 18rem;">
+  <!-- <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">Create a new badge </h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
     <a href="#" class="btn btn-dark">Create badge </a>
   </div>
-</div>
+</div> -->
 
-<!----------------------------------------------FOOTER------------------------------------------------------>
-
-<?php 
-    include("../components/footer.php");
-?>
-
+  <!----------------------------------------------FOOTER------------------------------------------------------>
 
 
 </body>
+
 </html>
