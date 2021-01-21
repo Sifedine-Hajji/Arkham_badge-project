265ff998457f53668e83983ef4aace6483b43b6f
denababaie <72995019+denababaie@users.noreply.github.com>
2021-01-21 14:48:36 +0100

    dashboard player added

---
+++ pages/dashboardplayer.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../assets/style.css" rel="stylesheet">
+    <title> Dashboard </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+<!--------------------------------------------NAVBAR------------------------------------------------------->
+
+<?php 
+    include("../components/navbar.php");
+?>
+
+<!------------------------------------------IMAGE--ON-TOP------------------------------------------------>
+
+<div class= dashboardbatmanimage>
+
+<div class= dashboardbatmanfilter>
+</div>
+</div>
+
+<!-------------------------------------------PROFILE-&-IMAGE--PLAYER-------------------------------------------------->
+
+<div class= profileplayer>
+<img src="../assets/images/gamerpic.jpg" id=gamerprofilepic>
+<p class=textaboutbadges> All your acquired <br> badges </p>
+</div>
+
+
+<!------------------------------------------LIST--OF-PLAYERS'-BADGES------------------------------------------------>
+
+<ul class="list-group" id=listofbadgess>
+   
+    <li class="playerbadges"> <img src="../assets/images/20.jpg">  Badge
+    </li>
+   
+    <li class="playerbadges"> <img src="../assets/images/20.jpg">  Badge
+    </li>
+   
+    <li class="playerbadges"> <img src="../assets/images/20.jpg">  Badge
+    </li>
+   
+    <li class="playerbadges"> <img src="../assets/images/20.jpg">  Badge
+    </li>
+   
+    <li class="playerbadges"> <img src="../assets/images/20.jpg">  Badge
+    </li>
+    
+</ul>
+
+<!------------------------------------------ALL-THE-BADGES-LISTED----------------------------------------------->
+
+
+
+<div class="row py-1" >
+    <div class="col-lg-9 mx-auto" >
+
+      <div class="card mb-4" >
+        <div class="card-body p-5">
+          <h4 class="mb-4" id= titleallbadges>All badges </h4>
+          <div class="custom-scrollbar-css p-5" >
+          
+
+            <div class= listofbadges>
+            <div class=allbadges>
+            <img src="../assets/images/20.jpg" id=imgbadge>
+            <h3 class=namebadge> Badgename </h3>
+            <p class= descriptionbadge> About the badge </p>
+            </div>
+
+            <div class=allbadges>
+            <img src="../assets/images/20.jpg" id=imgbadge>
+            <h3 class=namebadge> Badgename </h3>
+            <p class= descriptionbadge> About the badge </p>
+            </div>
+
+            <div class=allbadges>
+            <img src="../assets/images/20.jpg" id=imgbadge>
+            <h3 class=namebadge> Badgename </h3>
+            <p class= descriptionbadge> About the badge </p>
+            </div>
+
+            </div>
+
+
+</div>
+</div>
+
+        </div>
+        </div>
+      </div>
+      
+<!----------------------------------------------FOOTER------------------------------------------------------>
+
+
+
+
+
+</body>
+</html>
