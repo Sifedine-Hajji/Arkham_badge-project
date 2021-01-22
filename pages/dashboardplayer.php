dbc62a13f9faf89b232173c0d0276d6c04e06be8
denababaie <72995019+denababaie@users.noreply.github.com>
2021-01-22 11:33:53 +0100

    navbar changes

--- pages/dashboardplayer.php
+++ pages/dashboardplayer.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="../assets/style.css" rel="stylesheet">
+    <link rel="icon" href="../assets/favicon.ico" />
     <title> Dashboard </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
@@ -11,9 +12,14 @@
 
 <!--------------------------------------------NAVBAR------------------------------------------------------->
 
-<?php 
-    include("../components/navbar.php");
-?>
+<div class="sidenav">
+    <div id=logomenu> <img src="../assets/images/logo.png">  </div>
+    <div class="menubutton">  <a href="dashboardplayer.php"> <img src="../assets/images/homebutton.png"></a> </div>
+
+    <div id=logoutbutton> <a href="../pages/login.php"> <img src="../assets/images/logoutbutton.png"> </a>  </div>
+
+</div>
+
 
 <!------------------------------------------IMAGE--ON-TOP------------------------------------------------>
 
@@ -27,7 +33,9 @@
 
 <div class= profileplayer>
 <img src="../assets/images/gamerpic.jpg" id=gamerprofilepic>
-<p class=textaboutbadges> All your acquired <br> badges </p>
+
+<p class=textaboutbadges> 
+These are all your acquired <br> badges </p>
 </div>
 
 
@@ -60,7 +68,7 @@
     <div class="col-lg-9 mx-auto" >
 
       <div class="card mb-4" >
-        <div class="card-body p-5">
+        <div class="card-body p-5" id=colorbg>
           <h4 class="mb-4" id= titleallbadges>All badges </h4>
           <div class="custom-scrollbar-css p-5" >
           
